137a1dbcaebfd2f68cfc42a69cb133daa13f72f7
Lachlan <lachlan@lachlanb.com>
2021-10-14 15:58:28 +1100

    created test to find best priced dress

--- src/sample/HelloSelenium.java
+++ src/sample/HelloSelenium.java
@@ -1,5 +1,6 @@
 package sample;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,33 +10,29 @@ import java.util.*;
 import sample.homepage.HomePage;
 import sample.dressesPage.*;
 
+
 public class HelloSelenium {
 
     static WebDriver driver;
 
     public static void main(String[] args){
-        //Learn how to navigate iframes
-        //Learn how to use implicit and explicit waits
+
         System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number");
-        System.out.println("1. POM TEST");
-
-        //int input = sc.nextInt();
-        int input = 1;
 
         driver = new ChromeDriver();
 
-        if (input == 1){
-            //How do I define a page when I have multiple pages?
-            HomePage homePage = new HomePage();
-            homePage.clickDressesButton(driver);
-            DressesPage dressesPage = new DressesPage();
-            dressesPage.getAllDresses(driver);
-            //List<WebElement> dre
+        HomePage homePage = new HomePage();
+        homePage.clickDressesButton(driver);
+        DressesPage dressesPage = new DressesPage();
+        dressesPage.getAllDresses(driver);
+        List<WebElement> dresses = dressesPage.getAllDresses(driver);
+        for (WebElement dress : dresses){
+            System.out.println(dress.getText());
+            Boolean hasDiscount = dress.findElements(By.className("price-percent-reduction")).size() > 0;
+            if(hasDiscount) {
+                //String discount = dress.findElement(By.className("price-box")).getText();
+                System.out.println(dress.toString());
+            }
         }
     }
-
-
 }
